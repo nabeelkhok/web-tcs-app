2d7735c3e7a9e669752e29db1f393fb12f2a97da
nabeelkhok <161445085+nabeelkhok@users.noreply.github.com>
2025-06-04 23:55:41 +0500

    Create firebaseconfig.js

---
+++ config/firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmsgiHyTryt9AffjijcUoe2l9EiMTT414",
+  authDomain: "web-tcs-app-main.firebaseapp.com",
+  projectId: "web-tcs-app-main",
+  storageBucket: "web-tcs-app-main.firebasestorage.app",
+  messagingSenderId: "893027850083",
+  appId: "1:893027850083:web:f9bfa00958c11046e1e556"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
